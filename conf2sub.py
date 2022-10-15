c554a7b809cc4121f1023e0f5e622a4d22fdfe46
flowercoder <5116827+flowercoder@user.noreply.gitee.com>
2022-10-16 00:40:15 +0800

    init

---
+++ conf2sub.py
@@ -0,0 +1,21 @@
+
+# input config
+# read config file and read it.
+file = open("config.json", "r")
+lineofnum = len(file.readlines())
+print(lineofnum)
+file2 = open("config.json", "r")
+for i in range(lineofnum):
+    print(file2.readline())
+file.close()
+
+# Get what you need
+
+
+# build a url
+
+
+# trans the url to subscribble content
+
+# output sub
+# print the result on screen
