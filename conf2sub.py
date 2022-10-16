dc5f83eb08c8f959d8bbeecb64716be941fe019f
flowercoder <5116827+flowercoder@user.noreply.gitee.com>
2022-10-16 15:49:06 +0800

    init

--- conf2sub.py
+++ conf2sub.py
@@ -1,21 +1,45 @@
 
+
+import json
+import base64
+
 # input config
 # read config file and read it.
+
 file = open("config.json", "r")
-line = file.readline()
-while line:
-    current_line = line.strip()
-    print (current_line)
-    line = file.readline()
-
-file.close()
+allcontents = json.load(file)
 # Get what you need
-
+apart = allcontents.get('inbounds')[0].get('protocol')
+bpart = allcontents.get('inbounds')[0].get('settings').get('clients')[0].get('id')
+cpart = allcontents.get('inbounds')[0].get('streamSettings').get('xtlsSettings').get('serverName')
+dpart = allcontents.get('inbounds')[0].get('port')
+epart = allcontents.get('inbounds')[0].get('settings').get('decryption')
+fpart = allcontents.get('inbounds')[0].get('settings').get('clients')[0].get('flow')
+gpart = allcontents.get('inbounds')[0].get('streamSettings').get('security')
+hpart = cpart
+ipart = allcontents.get('inbounds')[0].get('streamSettings').get('network')
+jpart = cpart
+# print(apart)
+# print(bpart)
+# print(cpart)
+# print(dpart)
+# print(epart)
+# print(fpart)
+# print(gpart)
+# print(hpart)
+# print(ipart)
+# print(jpart)
+file.close()
 
 # build a url
-
-
+url = f"{apart}://{bpart}@{cpart}:{dpart}?encryption={epart}&flow={fpart}&security={gpart}&sni={hpart}&type={ipart}#{jpart}"
+# print(url)
 # trans the url to subscribble content
-
-# output sub
+# tobase64
+burl = bytes(url, encoding = "utf8")
+enurl = base64.urlsafe_b64encode(burl)
+deurl = base64.urlsafe_b64decode(enurl)
+# print(enurl)
+# print(deurl)
 # print the result on screen
+print(str(enurl)[2:-1])
